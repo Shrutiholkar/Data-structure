092c7249c46cb4436beec5c9bc13bfc9a095ed51
Shruti Holkar <138362092+Shrutiholkar@users.noreply.github.com>
2023-07-22 22:03:18 +0530

    Add files via upload

---
+++ m4.c
@@ -0,0 +1,35 @@
+//accept n numbers in an array and accept a number to search (using binary search)
+
+#include<stdio.h>
+int main()
+{
+int a[100],i,n,num,flag=0,top,mid,bottom;
+printf("enter limit");
+scanf("%d",&n);
+printf("enter n nos in sorted order");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter no to search");
+scanf("%d",&num);
+
+top=0;
+bottom=n-1;
+while(top<=bottom)
+{
+mid=(top+bottom)/2;
+if(a[mid]==num)
+{
+flag=1;break;
+}
+if(num>a[mid])
+top=mid+1;
+else
+bottom=mid-1;
+}
+if(flag==1)
+printf("no is found");
+else
+printf("no is not found");
+}
